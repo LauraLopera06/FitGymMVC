ada355cbf7a4e977d92a4fc13183eeeee08ccbab
LauraLopera06 <lala0604lm@gmail.com>
2025-04-27 20:26:13 -0500

    Agregué pagina de ayuda

--- FitGymMVC/Controllers/UsuariosController.cs
+++ FitGymMVC/Controllers/UsuariosController.cs
@@ -63,6 +63,11 @@ namespace FitGymMVC.Controllers
         {
             return View();
         }
+
+        public IActionResult Ayuda()
+        {
+            return View();
+        }
     }
 
 }
